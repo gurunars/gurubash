9362bb8c68f1c30304223b776e7aaedb80d1a7ce
Anton Berezin <anton.berezin@nokia.com>
2011-03-29 13:51:09 +0300

    Made installation script output more reasonable.

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ DIR_NAME=$(cd `dirname $0` && pwd)
 FILES=$DIR_NAME/*
 for old_name in $FILES
 do
-    if [ $old_name != ./install.sh ]; then
-        echo "Installing $old_name"
+    if [ $old_name != $0 ]; then
         new_name=.${old_name#$DIR_NAME/}
+        echo "Installed $HOME/$new_name"
         cp $old_name $HOME/$new_name
+
     fi
 done
 echo "Personal config files were successfully installed."
