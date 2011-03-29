e59ce9cffedd623644a4c1edf41685bcab85681e
Anton Berezin <anton.berezin@nokia.com>
2011-03-29 13:58:56 +0300

    Install file fix.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR_NAME=$(cd `dirname $0` && pwd)
 FILES=$DIR_NAME/*
 for old_name in $FILES
 do
-    if [ $old_name != $0 ]; then
+    if [ $old_name != "$DIR_NAME/install.sh" ]; then
         new_name=.${old_name#$DIR_NAME/}
         echo "Installed $HOME/$new_name"
         cp $old_name $HOME/$new_name
