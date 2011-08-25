cfecdf7513a96aa2327ead4b9b755ff39234a217
Anton Berezin <anton.berezin@nokia.com>
2011-08-25 17:31:39 +0300

    Install all dev packages and create home repo.

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install all the packages required for proper work
+
+sudo apt-get install -y debhelper git-buildpackage git-core dpkg-dev vim-nox \
+                        ipython pylint python-django
+
+# Install all the settings for home directory
+
 DIR_NAME=$(cd `dirname $0` && pwd)/home_settings
 FILES=$DIR_NAME/*
 for old_name in $FILES
@@ -9,3 +16,15 @@ do
     cp $old_name $HOME/$new_name
 done
 echo "Personal config files were successfully installed."
+
+# Create a home repositoory if it does not exist
+
+REPO=$HOME/deb_repo
+
+if ! grep $REPO /etc/apt/sources.list > /dev/null; then
+    mkdir $REPO
+    dpkg-scanpackages $REPO /dev/null | gzip -9c > $REPO/Packages.gz
+    sudo sh -c 'echo "# Home repo" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "deb file:$HOME/deb_repo /" >> /etc/apt/sources.list'
+    echo "Home repo created."
+fi
