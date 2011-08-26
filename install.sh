ccaf7f9fa45c741e3b934e4b592e7a66cdae9bf0
Anton Berezin <anton.berezin@nokia.com>
2011-08-26 10:10:34 +0300

    Got rid of time consuming operations.

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Install all the packages required for proper work
-
-sudo apt-get install -y debhelper git-buildpackage git-core dpkg-dev vim-nox \
-                        ipython pylint python-django dput
-
 # Install all the settings for home directory
 
 DIR_NAME=$(cd `dirname $0` && pwd)/home_settings
@@ -16,15 +11,3 @@ do
     cp $old_name $HOME/$new_name
 done
 echo "Personal config files were successfully installed."
-
-# Create a home repositoory if it does not exist
-
-REPO=$HOME/deb_repo
-
-if ! grep $REPO /etc/apt/sources.list > /dev/null; then
-    mkdir $REPO
-    dpkg-scanpackages $REPO /dev/null | gzip -9c > $REPO/Packages.gz
-    sudo sh -c 'echo "# Home repo" >> /etc/apt/sources.list'
-    sudo sh -c 'echo "deb file:$HOME/deb_repo /" >> /etc/apt/sources.list'
-    echo "Home repo created."
-fi
