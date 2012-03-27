4af71b09ab59e8f213e62626f30f00577d559c62
Anton Berezin <anton.berezin@f-secure.com>
2012-03-27 11:25:07 +0300

    Added prepare-kubuntu.sh

---
+++ prepare-kubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get remove dragonplayer amarok bluedevil kmail kopete ktorrent quassel kppp
+apt-get install git vim-nox pylint debhelper devscripts python-setuptools
+apt-get install python-support ipython yakuake pidgin chromium-browser dia vls
+apt-get autoremove
+apt-get upgrade
