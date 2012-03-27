d4737f1c29fb531f813855d6fec0213fb611a933
Anton Berezin <anton.berezin@f-secure.com>
2012-03-27 11:27:06 +0300

    Get rid of akregator.

--- prepare-kubuntu.sh
+++ prepare-kubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get remove dragonplayer amarok bluedevil kmail kopete ktorrent quassel kppp
+apt-get remove dragonplayer amarok bluedevil kmail kopete ktorrent quassel kppp akregator
 apt-get install git vim-nox pylint debhelper devscripts python-setuptools
 apt-get install python-support ipython yakuake pidgin chromium-browser dia vls
 apt-get autoremove
