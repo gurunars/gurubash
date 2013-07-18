cae90f2054ddd1687cacb26bf97797a33716e552
Anton Berezin <gurunars@gmail.com>
2013-07-18 23:17:08 +0300

    Updated the install script

--- prepare-kubuntu.sh
+++ prepare-kubuntu.sh
@@ -2,7 +2,6 @@
 
 apt-get update
 apt-get remove dragonplayer amarok bluedevil kopete ktorrent quassel kppp akregator
-apt-get install git vim-nox pylint debhelper devscripts python-setuptools
-apt-get install python-support ipython yakuake pidgin chromium-browser dia vls
+apt-get install vim-nox pylint debhelper devscripts python-setuptools python-support ipython yakuake
 apt-get autoremove
 apt-get upgrade
