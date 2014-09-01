4b77d82413beb50e3e3e99727c398351c53d84bf
Anton Berezin <anton.berezin@f-secure.com>
2014-09-01 09:56:10 +0300

    ff

--- prepare-kubuntu.sh
+++ prepare-kubuntu.sh
@@ -2,6 +2,8 @@
 
 apt-get update
 apt-get remove dragonplayer amarok bluedevil kopete ktorrent quassel kppp akregator
-apt-get install vim-nox pylint debhelper devscripts python-setuptools python-support ipython yakuake
+apt-get install vim-nox pylint debhelper devscripts python-setuptools ipython yakuake python-pip
 apt-get autoremove
 apt-get upgrade
+
+pip install nose mock rednose virtualenv
