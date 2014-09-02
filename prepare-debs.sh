2eb0b3ea1322e3c4fc784aaaa6c2c003fac92607
Anton Berezin <anton.berezin@f-secure.com>
2014-09-02 09:14:34 +0300

    ff

--- prepare-debs.sh
+++ prepare-debs.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get remove dragonplayer amarok bluedevil kopete ktorrent quassel kppp akregator
-apt-get install vim-nox pylint debhelper devscripts python-setuptools ipython yakuake python-pip
+apt-get install vim-nox pylint debhelper devscripts python-setuptools ipython yakuake python-pip deb-dev
 apt-get autoremove
 apt-get upgrade
 
