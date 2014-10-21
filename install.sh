e507560fe615718dc4b25316f8a47c310c1cf483
Anton Berezin <anton.berezin@f-secure.com>
2014-10-21 16:24:12 +0300

    Moved color defenitions into a separate folder

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ for old_name in $FILES
 do
     new_name=.${old_name#$DIR_NAME/}
     echo "Installed $HOME/$new_name"
-    cp $old_name $HOME/$new_name
+    cp -r $old_name $HOME/$new_name
 done
 echo "Personal config files were successfully installed."
 
--- prepare-debs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get remove dragonplayer amarok bluedevil kopete ktorrent quassel kppp akregator
-apt-get install vim-nox pylint debhelper devscripts python-setuptools ipython yakuake python-pip deb-dev
-apt-get autoremove
-apt-get upgrade
-
-pip install nose mock rednose virtualenv githooks
