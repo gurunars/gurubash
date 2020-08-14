20c53bb25bc2110595ad49b8c1966dfbdf5bb12e
__NAME <gurunars@gmail.com>
2020-08-14 13:58:25 +0200

    ff

---
+++ chrome/install.sh
@@ -0,0 +1,2 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/chrome.deb
+sudo dpkg -i /tmp/chrome.deb
--- code/install.sh
+++ code/install.sh
@@ -12,8 +12,8 @@ VS_PATH=${HOME}/.config/Code/User
 
 mkdir -p ${VS_PATH}
 
-ln -f -s ~/.gurubash/vscode/keybindings.json ${VS_PATH}/keybindings.json
-ln -f -s ~/.gurubash/vscode/settings.json ${VS_PATH}/settings.json
+ln -f -s ~/.gurubash/code/keybindings.json ${VS_PATH}/keybindings.json
+ln -f -s ~/.gurubash/code/settings.json ${VS_PATH}/settings.json
 
 # extensions
 
