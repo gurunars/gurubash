9d82198a5dfd038955b2a579f376919e029d4577
Anton Berezin <gurunars@gmail.com>
2020-08-22 14:43:18 +0200

    ff

---
+++ apt/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt dist-upgrade
+mkdir -p /etc/apt/sources.list.d
---
+++ chrome/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+sudo apt install google-chrome-stable
