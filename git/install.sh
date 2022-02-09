5dd419de902ab851a8ec7a183fded719ea3ff449
Anton Berezin <gurunars@gmail.com>
2022-02-09 15:36:49 +0100

    work folder

--- git/install.sh
+++ git/install.sh
@@ -6,5 +6,10 @@ ln -f -s ~/.gurubash/git/gitconfig ~/.gitconfig
 
 . ~/.gurubash/personal/loader
 
-git config --global user.name "${NAME}"
-git config --global user.email "${EMAIL}"
+mkdir ~/repo
+git config -f ~/repo/gitconfig user.name "${NAME}"
+git config -f ~/repo/gitconfig user.email "${EMAIL}"
+
+mkdir ~/work
+git config -f ~/work/gitconfig user.name "${NAME}"
+git config -f ~/work/gitconfig user.email "${EMAIL}"
