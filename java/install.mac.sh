9382742d49d2ab8ec2e74613f04f5f1eeb4660ec
Anton Berezin <gurunars.com>
2023-12-20 22:04:04 +0100

    ff

--- java/install.mac.sh
+++ java/install.mac.sh
@@ -1,1 +1,10 @@
 APP_NAME=java $INSTALL
+
+LOCATION=/Library/Java/JavaVirtualMachines/openjdk.jdk
+
+if [ -e $LOCATION ]; then
+    echo "OpenJDK is already configured"
+else
+    sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk \
+      $LOCATION
+fi
